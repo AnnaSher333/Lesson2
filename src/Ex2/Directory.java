ebc21c3d6a7915a51a11191ea633281698b179da
annasheremet <aksanna333@icloud.com>
2024-12-11 22:31:31 +0300

    add idea to ignore Внесены правки в соответствие с замечанием - переписан класс Directory в основу взят Set для хранения телефонов и Map для справочника.

--- src/Ex2/Directory.java
+++ src/Ex2/Directory.java
@@ -13,47 +13,29 @@ import static java.util.Collections.emptyList;
 тогда при запросе такой фамилии должны выводяться все телефоны.
 */
 public class Directory {
-    private String surname; //фамилия
-    private String phone; //телефон
-    private static List<Directory> direct = new ArrayList<>(); //список контактов
+    private static Map<String, Set<String>> direct; //список контактов
 
     public Directory(){
-    }
-    public Directory(String surname, String phone){
-        this.surname = surname;
-        this.phone = phone;
-    }
-
-    public String getPhone() {
-        return phone;
+        direct = new HashMap<>();
     }
 
     public void add(String surname, String phone){ // добавить контакт
-        direct.add(new Directory(surname, phone));
+        Set<String> phones = direct.getOrDefault(surname, new HashSet<>());
+        phones.add(phone);
+        direct.put(surname, phones);
     }
 
-    public List<String> get(String surname){ // найти телефон по фамилии
-        List<String> list = new ArrayList<>();
-            if (direct.isEmpty()) { //на случай, если справочник пустой
+    public Set<String> get(String surname){ // найти телефоны по фамилии
+            if (direct.isEmpty()) { //если справочник пустой
                 System.out.println("Справочник пуст, добавьте контакт");
-            } else if (uniqSurname().contains(surname)) { //если фамилия есть
-                List<String> list2 = new ArrayList<>();
-                System.out.println("Телефоны контакта " + surname + ":");
-                direct.stream().filter(s -> s.surname.equals(surname))
-                        .forEach(s -> list2.add(s.getPhone()));
-                list = list2;
-            } else { // если фамилии нет
+                return null;
+            } else if (!direct.containsKey(surname)) { //если фамилии нет
                 System.out.println("Контакт с фамилией " + surname + " отсутствует");
-                list = Collections.emptyList();
+                return Collections.emptySet();
+            } else { // если фамилия есть
+                System.out.println("Номера контакта с фамилией " + surname + ":");
+                return direct.get(surname);
             }
-            return list;
-    }
-
-    //Вспомогательный метод проверка: список уникальных фамилий
-    public Set<String> uniqSurname(){
-        Set<String> set = new HashSet<>();
-        direct.stream().forEach(d -> set.add(d.surname));
-        return set;
     }
 }
 
